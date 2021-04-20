275e3bf36b36c6735317e64a141a62bd6152223e
zhengx55 <913563967@qq.com>
2021-04-20 14:08:08 +0800

    test file methods #1

--- src/test/EthSwap.test.js
+++ src/test/EthSwap.test.js
@@ -1,1 +1,19 @@
+const { assert } = require('chai');
+
 // truffle bundle with mocha framework to create test suites
+const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
+// config chai dependencies
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('EthSwap', (accounts) => {
+    describe('EthSwap deployment', async () => {
+        it('contract has a name', async () =>{
+            let ethSwap = await EthSwap.new();
+            const name = await ethSwap.name();
+            assert.equal(name, 'EthSwap Instant Exchange');
+        })
+    })
+})
